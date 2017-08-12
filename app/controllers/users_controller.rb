7844826bfcd839bf179bf851cc2f76603a9e5d8d
Leah Dudley <lstilphen@spoonflower.com>
2017-08-12 15:45:10 -0400

    validations in user model and new user form is submitting properly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,35 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
 
   def create
     @user = User.new(user_params)
-    @user.save
-    redirect_to @user
+    if @user.save
+      flash[:notice] = "User was successfully created."
+      redirect_to @user
+    else
+      flash[:error] = "User could not be created."
+      render 'new'
+    end
   end
 
   def show
     @user = User.find(params[:id])
   end
 
+  def edit
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :username, :password, :email_address)
+    params.require(:user).permit(:first_name, :last_name, :email_address, :username, :password, :password_confirmation)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :first_name, presence: true
+  validates :email_address, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }, confirmation: true
+  validates :password_confirmation, presence: true
+
 end
